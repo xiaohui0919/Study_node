e106e43d462c9cd08023d4b037855ab84825c8e1
zhengxh <18618113932@163.com>
2017-10-26 18:12:06 +0800

    'GET/POST请求'

--- GETorPOST/get.js
+++ GETorPOST/get.js
@@ -2,3 +2,17 @@
  * Written by:     zhengxh
  * Created Date:   2017/10/26
  */
+var http=require('http')
+var url=require('url')
+var util=require('util')
+
+http.createServer(function (req,res) {
+    res.writeHead(200,{'Content-Type':'text/plain;charset=utf-8'})
+    // 解析 url 参数
+    var params=url.parse(req.url,true).query;
+    res.write('网络名：'+params.name)
+    res.write('\n')
+    res.write('网络url：'+params.url)
+    res.write('\n')
+    res.end(util.inspect(url.parse(req.url,true)))
+}).listen(3000)
--- GETorPOST/post.js
+++ GETorPOST/post.js
@@ -2,3 +2,37 @@
  * Written by:     zhengxh
  * Created Date:   2017/10/26
  */
+var http=require('http')
+var querystring=require('querystring')
+
+var postHTML =
+    '<html><head><meta charset="utf-8"><title></title></head>' +
+    '<body>' +
+    '<form method="post">' +
+    '网站名： <input name="name"><br>' +
+    '网站 URL： <input name="url"><br>' +
+    '<input type="submit">' +
+    '</form>' +
+    '</body></html>';
+
+http.createServer(function (req,res) {
+    var post='';
+    req.on('data',function (chunk) {
+        post+=chunk
+    })
+    req.on('end',function () {
+        // 解析参数
+        post=querystring.parse(post)
+        // 设置响应头部信息及编码
+        res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'})
+
+        if (post.name&&post.url){//输出提交的数据
+            res.write('网站名：'+post.name)
+            res.write('<br>')
+            res.write('网站url：'+post.url)
+        }else {//输出表单
+            res.write(postHTML)
+        }
+        res.end()
+    })
+}).listen(3000)
