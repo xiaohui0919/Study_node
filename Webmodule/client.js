c4a5ed66e253842e2b8d6c91a49d20520f375043
zhengxh <18618113932@163.com>
2017-10-27 16:31:45 +0800

    'WEB模块'

---
+++ Webmodule/client.js
@@ -0,0 +1,30 @@
+/**
+ * Written by:     zhengxh
+ * Created Date:   2017/10/27
+ */
+var http=require('http')
+
+// 用于请求的选项
+var options={
+    host:'localhost',
+    port:'8081',
+    path:'/Webmodule/index.html'
+}
+
+// 处理响应的回调函数
+var callback=function (response) {
+    // 不断更新数据
+    var body='';
+    response.on('data',function (data) {
+        body+=data;
+    })
+
+    response.on('end',function () {
+        //数据接收完成
+        console.log(body);
+    })
+}
+
+// 向服务端发送请求
+var req=http.request(options,callback);
+req.end();
